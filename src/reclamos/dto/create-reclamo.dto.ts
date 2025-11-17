691447d1be7c6768b90ddd4069c6ecc7a119b5a4
matibbaez <matiascaneda3@gmail.com>
2025-11-17 17:16:31 -0300

    Implementar validación (blindaje) de DNI y Nombre en el DTO

--- src/reclamos/dto/create-reclamo.dto.ts
+++ src/reclamos/dto/create-reclamo.dto.ts
@@ -1,15 +1,31 @@
-import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+import {
+  IsString,
+  IsEmail,
+  IsNotEmpty,
+  MinLength,
+  MaxLength,
+  Matches,
+  IsNumberString,
+} from 'class-validator';
 
 export class CreateReclamoDto {
-  @IsString()   
-  @IsNotEmpty() 
-  nombre: string;
-
+  
   @IsString()
   @IsNotEmpty()
+  @MinLength(3) 
+  // 2. ¡Regex! Solo letras (incluyendo acentos y 'ñ') y espacios
+  @Matches(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]*$/, {
+    message: 'El nombre solo puede contener letras y espacios',
+  })
+  nombre: string;
+
+  @IsNotEmpty()
+  @IsNumberString({}, { message: 'El DNI solo puede contener números' }) 
+  @MinLength(7)
+  @MaxLength(8) 
   dni: string;
 
-  @IsEmail()    
+  @IsEmail()
   @IsNotEmpty()
   email: string;
 }
