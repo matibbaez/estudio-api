1a42aa94a13049de734f06a4e7388a0594ed5135
matibbaez <matiascaneda3@gmail.com>
2025-12-09 15:36:54 -0300

    Se agrega tipo tramite

--- src/reclamos/entities/reclamo.entity.ts
+++ src/reclamos/entities/reclamo.entity.ts
@@ -5,9 +5,9 @@ import {
   CreateDateColumn,
 } from 'typeorm';
 
-@Entity('reclamos') // <-- Este será el nombre de la tabla en MySQL
+@Entity('reclamos') 
 export class Reclamo {
-  @PrimaryGeneratedColumn('uuid') // <-- Usamos 'uuid' para generar un ID de texto
+  @PrimaryGeneratedColumn('uuid') 
   id: string;
 
   @Column({ type: 'varchar', length: 255 })
@@ -19,7 +19,7 @@ export class Reclamo {
   @Column({ type: 'varchar', length: 100 })
   email: string;
 
-  @Column({ type: 'varchar', length: 10, unique: true }) // <-- ¡único!
+  @Column({ type: 'varchar', length: 10, unique: true }) 
   codigo_seguimiento: string;
 
   @Column({
@@ -29,16 +29,14 @@ export class Reclamo {
   })
   estado: string;
 
-  // --- Paths a los archivos en la Bóveda (Supabase) ---
-  // (Los guardamos como texto)
   @Column({ type: 'text' })
   path_dni: string;
 
   @Column({ type: 'text' })
   path_recibo: string;
 
-  @Column({ type: 'text', nullable: true }) // <-- nullable: true = opcional
-  path_alta_medica: string | null; // <-- ¡LE AGREGAMOS EL | null !
+  @Column({ type: 'text', nullable: true }) 
+  path_alta_medica: string | null; 
 
   @Column({ type: 'text' })
   path_form1: string;
@@ -46,6 +44,18 @@ export class Reclamo {
   @Column({ type: 'text' })
   path_form2: string;
 
-  @CreateDateColumn() // <-- TypeORM pone la fecha de creación automáticamente
+  @CreateDateColumn() 
   fecha_creacion: Date;
+
+  @Column({ nullable: true })
+  tipo_tramite: string; // 'Alta Medica', 'Rechazo', 'Prestaciones'
+
+  @Column({ nullable: true })
+  subtipo_tramite: string; // 'Div. Alta', 'Div. Prestaciones', 'Reingreso' (Solo si es Prestaciones)
+
+  @Column({ nullable: true })
+  path_carta_documento: string; // Solo si es Rechazo
+
+  @Column({ nullable: true })
+  path_revoca_patrocinio: string; // Solo si vino del banner especial
 }
